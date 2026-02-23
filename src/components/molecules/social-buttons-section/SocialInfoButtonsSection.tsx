6b2b4b64ad17086c076dc027b04a050c237d2bcb
Sachin Rupani <sachin.rupani1992@gmail.com>
2026-02-23 18:45:58 +0530

    fix: Updated resume link

--- src/components/molecules/social-buttons-section/SocialInfoButtonsSection.tsx
+++ src/components/molecules/social-buttons-section/SocialInfoButtonsSection.tsx
@@ -19,7 +19,7 @@ const SocialInfoButtonsSection = () => {
       <IconButton
         title="Download Resume"
         iconSrc="/ic_download.svg"
-        href="https://drive.google.com/file/d/1Sqplk9ejz9_HM6Ivxr-orULPCk1kKoDZ/view?usp=sharing"
+        href="https://drive.google.com/file/d/1tQTe2TXARt9fQReb2SryIf6mYaTThACH/view?usp=drive_link"
       />
     </div>
   );
